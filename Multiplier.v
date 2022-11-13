b0bba8f278dce54c2ee6019922fecc4101891998
Ali Badry abd elnaeem <alibadry_2000@hotmail.com>
2022-11-13 14:25:05 +0200

    fault during committing

--- Multiplier.v
+++ Multiplier.v
@@ -5,38 +5,22 @@ module Multiplier #(
     output reg [WIDTH-1:0]  out
 );
 reg [2*WIDTH-1:0] out_2WIDTH;
-reg [WIDTH-1:0] abs_in1, abs_in2;
+reg [WIDTH-1:0] digit_product, mixed_product1, mixed_product2, decimal_product;
 
 always @(*) 
 begin
-    if (in1<0)
+    if ((in1<0 && in2>0) || (in>0 && in2<0))
     begin
-        abs_in1 = -in1;
+        digit_product = in1[WIDTH-1:WIDTH>>1] * in2[WIDTH-1:WIDTH>>1];
+        mixed_product1 = in1[(WIDTH>>1)-1:0] * in2[WIDTH-1:WIDTH>>1] + in2[(WIDTH>>1)-1:0] * in1[WIDTH-1:WIDTH>>1];
+        decimal_product = in1[(WIDTH>>1)-1:0] * in2[(WIDTH>>1)-1:0];
+        out_2WIDTH = {digit_product + mixed_product1[WIDTH-1:WIDTH>>1], -decimal_product-mixed_product1[(WIDTH>>1)-1:0]};
     end
     else
     begin
-        abs_in1 = in1;
-    end
-    if (in2<0)
-    begin
-        abs_in2 = -in2;
-    end
-    else
-    begin
-        abs_in2 = in2;
+        out_2WIDTH = in1*in2;
     end
 
-    out_2WIDTH = abs_in1*abs_in2;
-
-    if (in1[WIDTH-1]^in2[WIDTH-1]) 
-    begin
-        out_2WIDTH = -out_2WIDTH;
-        out = out_2WIDTH [WIDTH+(WIDTH>>1)-1:WIDTH>>1];     
-    end
-    else
-    begin
-        out = out_2WIDTH [WIDTH+(WIDTH>>1)-1:WIDTH>>1];
-    end
+    out = out_2WIDTH [WIDTH+(WIDTH>>1)-1:WIDTH>>1];
 end
-    
 endmodule
